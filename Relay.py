c86af9f6e7016571d87939a3e692c8f73a17dba1
kunal patil <kunalpatil9858@gmail.com>
2025-06-23 15:39:38 +0530

    Update Relay.py

--- Relay.py
+++ Relay.py
@@ -1,14 +1,14 @@
 from machine import Pin
 from time import sleep
 
-# Connect relay IN pin or LED to GPIO 26 (you can change it)
+
 relay = Pin(13, Pin.OUT)
 
 while True:
-    relay.value(1)  # Turn ON relay or LED
+    relay.value(1) 
     print("Relay ON")
-    sleep(5)        # Wait for 5 seconds
+    sleep(5)        
 
-    relay.value(0)  # Turn OFF relay or LED
+    relay.value(0) 
     print("Relay OFF")
-    sleep(3)        # Wait for 3 seconds
+    sleep(3)        
